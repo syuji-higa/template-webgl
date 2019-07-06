8259b9d71107c3a133358cd85ac12c360e423203
syuji-higa <free@syuji-higa.com>
2019-07-06 17:21:13 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const { join } = require('path')
+const CopyPlugin = require('copy-webpack-plugin')
+
+const config = {
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(frag|vert|glsl)$/,
+        use: 'webpack-glsl-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.ts']
+  }
+}
+
+module.exports = (env, argv) => {
+  if (argv.mode === 'development') {
+    config.devtool = 'cheap-module-eval-source-map'
+    config.devServer = {
+      contentBase: join(__dirname, './src'),
+      watchContentBase: true
+    }
+  } else if (argv.mode === 'production') {
+    config.plugins = [
+      new CopyPlugin(
+        [
+          {
+            from: 'src/index.html',
+            force: true
+          },
+          {
+            from: 'src/*.+(png|jpg|gif)',
+            to: '[name].[ext]',
+            type: 'template',
+            force: true
+          }
+        ],
+        { copyUnmodified: true }
+      )
+    ]
+  }
+  return config
+}
